53b444398f7c2eaae8cd40ce9565f1c01c774b96
Justin Kumpe <jakumpe@kumpes.com>
2025-05-04 17:33:07 -0500

    fixed held for agency

--- scrapes/crawford_so_ar.py
+++ scrapes/crawford_so_ar.py
@@ -131,7 +131,7 @@ def scrape_crawford_so_ar(session: Session, jail: Jail, log_level: str = "INFO")
             arrest_date=arrest_date,
             jail_id=jail.jail_id,
             is_juvenile=False,
-            held_for=details["arresting_agency"],
+            held_for_agency=details["arresting_agency"],
             hold_for_reason=details["charges"],
         )
         inmates.append(inmate)
