0e2681b89bd372d9f9fabe3a69ec170736fbf08d
Justin Kumpe <jakumpe@kumpes.com>
2025-05-09 19:27:13 -0500

    added mugshot to washington

--- scrapes/crawford_so_ar.py
+++ scrapes/crawford_so_ar.py
@@ -11,33 +11,9 @@ from models.Jail import Jail
 from models.Inmate import Inmate
 
 from scrapes.process import process_scrape_data
+from helpers.image_helper import image_url_to_base64
 
 URL = "https://inmates.crawfordcountysheriff.org"
-
-
-def image_url_to_base64(image_url):
-    """
-    Fetches an image from a URL and returns its Base64 representation.
-
-    Args:
-        image_url: The URL of the image.
-
-    Returns:
-        A string containing the Base64 encoded image data, or None if an error occurs.
-    """
-    try:
-        response = requests.get(image_url, stream=True)
-        response.raise_for_status()  # Raise HTTPError for bad responses (4xx or 5xx)
-
-        image_data = response.content
-        base64_encoded = base64.b64encode(image_data).decode("utf-8")
-        return base64_encoded
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching image from URL: {e}")
-        return None
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return None
 
 
 def scrape_inmate_data(details_path: str) -> dict:
